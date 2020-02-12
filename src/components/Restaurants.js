22177ebedc92e738b9ea69317d1808634f7d685b
Ayron Felipe <ayron41@gmail.com>
2020-02-12 09:38:31 -0300

    Estilização dos cards de restaurante
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -8,8 +8,19 @@ function Restaurants() {
     return(
         <div className="restaurants">
             <h2 className="page-title">All Restaurants</h2>
+            <div className="cards">
+                <div className="card-restaurant">
+                    <div className="card-restaurant-image">
+                        <img src="//via.placeholder.com/304x228" alt="Image restaurant" style={{ width: '304px', height: '228px' }} />
+                    </div>
+                    <div className="card-restaurant-title">
+                        Very Long Name Restaurants Number 1 In List
+                    </div>
+                    <div className="card-restaurant-detail"></div>
+                </div>
+            </div>
         </div>
     );
-}
+};
 
 export default Restaurants;
