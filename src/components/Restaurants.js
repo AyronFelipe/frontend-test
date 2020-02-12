d697df744bb74144dda89552937e8b749510b3a6
Ayron Felipe <ayron41@gmail.com>
2020-02-12 09:53:46 -0300

    Botão de detalhe e ajustes nas estilizações
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -13,10 +13,16 @@ function Restaurants() {
                     <div className="card-restaurant-image">
                         <img src="//via.placeholder.com/304x228" alt="Image restaurant" style={{ width: '304px', height: '228px' }} />
                     </div>
-                    <div className="card-restaurant-title">
-                        Very Long Name Restaurants Number 1 In List
+                    <div className="card-restaurant-detail">
+                        <div className="card-restaurant-title">
+                            Very Long Name Restaurants Number 1 In List
+                        </div>
+                        <div style={{ paddingTop: '10px' }}>
+                            <button type="button" className="btn-primary">
+                                learn more
+                            </button>
+                        </div>
                     </div>
-                    <div className="card-restaurant-detail"></div>
                 </div>
             </div>
         </div>
