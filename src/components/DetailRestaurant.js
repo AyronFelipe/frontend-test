b1533cdc19c9ce1c251edc0b767bede717fc0388
Ayron Felipe <ayron41@gmail.com>
2020-02-12 14:59:44 -0300

    Página de detalhes do restaurante
    Signed-off-by: Ayron Felipe <ayron41@gmail.com>

--- src/components/DetailRestaurant.js
+++ src/components/DetailRestaurant.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import StarRatings from 'react-star-ratings';
+import "../styles/detail.css";
 
 function DetailRestaurant() {
 
     return(
-        <>
+        <div className="restaurant-detail">
             <h1 className="page-title">Restaurant 3</h1>
-            <StarRatings
-                rating={4}
-                starDimension={'20px'}
-                starSpacing={'1px'}
-                starRatedColor={'#002B56'} />
-        </>
+            <div className="restaurant-detail-stars">
+                <StarRatings
+                    rating={4}
+                    starDimension={'30px'}
+                    starSpacing={'1px'}
+                    starRatedColor={'#002B56'} />
+            </div>
+            <div className="restaurant-detail-meta">
+                <div className="category">
+                    <span>JAPANESE • $$$</span>
+                </div>
+                <div className="open">
+                    <p className="open-text"><span className="dot">•</span>open now</p>
+                </div>
+            </div>
+        </div>
     );
 }
 
